96a5ea28c14f361a6a47d1941317a86f9be397ad
litongmacos <litongjava@qq.com>
2023-11-13 22:37:02 -1000

    add code chunk

---
+++ cmake-cpp-base/print_system_information.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ping Lee on 2023/11/13.
+//
+
+#include <cstdio>
+
+int main() {
+  {
+    printf("%d\n", 0);
+  }
+  return 0;
+}
---
+++ cmake-cpp-base/print_system_information.h
@@ -0,0 +1,14 @@
+//
+// Created by Ping Lee on 2023/11/13.
+//
+
+#ifndef CMAKE_CPP_BASE_PRINT_SYSTEM_INFORMATION_H
+#define CMAKE_CPP_BASE_PRINT_SYSTEM_INFORMATION_H
+
+
+class print_system_information {
+
+};
+
+
+#endif //CMAKE_CPP_BASE_PRINT_SYSTEM_INFORMATION_H
