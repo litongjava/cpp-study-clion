7488381b57509ddb375aa21d604efaf89e74bc72
litongjava <litongjava@qq.com>
2023-11-14 02:29:54 -1000

    remove auto

--- cmake-boost-beast-http-demo/http_server.cpp
+++ cmake-boost-beast-http-demo/http_server.cpp
@@ -60,9 +60,8 @@ private:
     socket_.get_executor(), std::chrono::seconds(60)};
 
   // Asynchronously receive a complete request message.
-  void
-  read_request() {
-    auto self = shared_from_this();
+  void read_request() {
+    const std::shared_ptr<http_connection> &self = shared_from_this();
 
     http::async_read(
       socket_,
@@ -77,8 +76,7 @@ private:
   }
 
   // Determine what needs to be done with the request message.
-  void
-  process_request() {
+  void process_request() {
     response_.version(request_.version());
     response_.keep_alive(false);
 
@@ -109,8 +107,7 @@ private:
   }
 
   // Construct a response message based on the program state.
-  void
-  create_response() {
+  void create_response() {
     if (request_.target() == "/count") {
       response_.set(http::field::content_type, "text/html");
       beast::ostream(response_.body())
@@ -144,8 +141,8 @@ private:
 
   void create_post_response() {
     if (request_.target() == "/email") {
-      auto &body = this->request_.body();
-      auto body_str = boost::beast::buffers_to_string(body.data());
+      beast::basic_multi_buffer<std::allocator<char>> &body = this->request_.body();
+      const std::string &body_str = boost::beast::buffers_to_string(body.data());
       std::cout << "receive body is " << body_str << std::endl;
       this->response_.set(http::field::content_type, "text/json");
       Json::Value root;
@@ -160,7 +157,7 @@ private:
         return;
       }
 
-      auto email = src_root["email"].asString();
+      const Json::String &email = src_root["email"].asString();
       std::cout << "email is " << email << std::endl;
 
       root["error"] = 0;
@@ -176,9 +173,8 @@ private:
   }
 
   // Asynchronously transmit the response message.
-  void
-  write_response() {
-    auto self = shared_from_this();
+  void write_response() {
+    const std::shared_ptr<http_connection> &self = shared_from_this();
 
     response_.content_length(response_.body().size());
 
@@ -192,9 +188,8 @@ private:
   }
 
   // Check whether we have spent enough time on this connection.
-  void
-  check_deadline() {
-    auto self = shared_from_this();
+  void check_deadline() {
+    const std::shared_ptr<http_connection> &self = shared_from_this();
 
     deadline_.async_wait(
       [self](beast::error_code ec) {
@@ -207,33 +202,24 @@ private:
 };
 
 // "Loop" forever accepting new connections.
-void
-http_server(tcp::acceptor &acceptor, tcp::socket &socket) {
-  acceptor.async_accept(socket,
-                        [&](beast::error_code ec) {
-                          if (!ec)
-                            std::make_shared<http_connection>(std::move(socket))->start();
-                          http_server(acceptor, socket);
-                        });
+void http_server(tcp::acceptor &acceptor, tcp::socket &socket) {
+// 定义回调函数
+  auto accept_handler = [&acceptor, &socket](beast::error_code ec) {
+    if (!ec) {
+      std::make_shared<http_connection>(std::move(socket))->start();
+    }
+    http_server(acceptor, socket);
+  };
+
+  // 使用定义的回调函数
+  acceptor.async_accept(socket, accept_handler);
 }
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+  const boost::asio::ip::address address = net::ip::make_address("127.0.0.1");
+  ushort port = static_cast<unsigned short>(8080);
+
   try {
-    // Check command line arguments.
-    /*if (argc != 3)
-    {
-      std::cerr << "Usage: " << argv[0] << " <address> <port>\n";
-      std::cerr << "  For IPv4, try:\n";
-      std::cerr << "    receiver 0.0.0.0 80\n";
-      std::cerr << "  For IPv6, try:\n";
-      std::cerr << "    receiver 0::0 80\n";
-      return EXIT_FAILURE;
-    }*/
-
-    auto const address = net::ip::make_address("127.0.0.1");
-    unsigned short port = static_cast<unsigned short>(8080);
-
     net::io_context ioc{1};
 
     tcp::acceptor acceptor{ioc, {address, port}};
