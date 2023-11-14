f17e7c2d43e1e0b0de7bcb4deba1bfcb93273c4c
litongmacos <litongjava@qq.com>
2023-11-14 11:24:31 -1000

    streamline code

--- cmake-nlohmann-json-learn/main.cpp
+++ cmake-nlohmann-json-learn/main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include "json.hpp"
-using json = nlohmann::json;
 int main() {
-
-  json jres;
   std::string results = "hello";
-  jres = json{{"content", results}};
-
-  auto json_string=jres.dump(-1, ' ', false, json::error_handler_t::replace);
-
+  nlohmann::json  json_object= nlohmann::json{{"content", results}};
+  auto json_string=json_object.dump(-1, ' ', false,nlohmann::json::error_handler_t::replace);
   std::cout << json_string << std::endl;
   return 0;
 }
