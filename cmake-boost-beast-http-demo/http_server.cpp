4f3ff928de6907b2ef0b9029e2985baa86468632
litongjava <litongjava@qq.com>
2023-11-14 02:05:18 -1000

    整合http-server

--- cmake-boost-beast-http-demo/http_server.cpp
+++ cmake-boost-beast-http-demo/http_server.cpp
@@ -8,43 +8,36 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <json/json.h>
-#include <json/value.h>
-#include <json/reader.h>
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/value.h>
+#include <jsoncpp/json/reader.h>
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-namespace my_program_state
-{
+namespace my_program_state {
   std::size_t
-  request_count()
-  {
+  request_count() {
     static std::size_t count = 0;
     return ++count;
   }
 
   std::time_t
-  now()
-  {
+  now() {
     return std::time(0);
   }
 }
 
-class http_connection : public std::enable_shared_from_this<http_connection>
-{
+class http_connection : public std::enable_shared_from_this<http_connection> {
 public:
   http_connection(tcp::socket socket)
-    : socket_(std::move(socket))
-  {
+    : socket_(std::move(socket)) {
   }
 
   // Initiate the asynchronous operations associated with the connection.
-  void
-  start()
-  {
+  void start() {
     read_request();
     check_deadline();
   }
@@ -54,7 +47,7 @@ private:
   tcp::socket socket_;
 
   // The buffer for performing reads.
-  beast::flat_buffer buffer_{ 8192 };
+  beast::flat_buffer buffer_{8192};
 
   // The request message.
   http::request<http::dynamic_body> request_;
@@ -64,12 +57,11 @@ private:
 
   // The timer for putting a deadline on connection processing.
   net::steady_timer deadline_{
-    socket_.get_executor(), std::chrono::seconds(60) };
+    socket_.get_executor(), std::chrono::seconds(60)};
 
   // Asynchronously receive a complete request message.
   void
-  read_request()
-  {
+  read_request() {
     auto self = shared_from_this();
 
     http::async_read(
@@ -77,8 +69,7 @@ private:
       buffer_,
       request_,
       [self](beast::error_code ec,
-             std::size_t bytes_transferred)
-      {
+             std::size_t bytes_transferred) {
         boost::ignore_unused(bytes_transferred);
         if (!ec)
           self->process_request();
@@ -87,13 +78,11 @@ private:
 
   // Determine what needs to be done with the request message.
   void
-  process_request()
-  {
+  process_request() {
     response_.version(request_.version());
     response_.keep_alive(false);
 
-    switch (request_.method())
-    {
+    switch (request_.method()) {
       case http::verb::get:
         response_.result(http::status::ok);
         response_.set(http::field::server, "Beast");
@@ -121,10 +110,8 @@ private:
 
   // Construct a response message based on the program state.
   void
-  create_response()
-  {
-    if (request_.target() == "/count")
-    {
+  create_response() {
+    if (request_.target() == "/count") {
       response_.set(http::field::content_type, "text/html");
       beast::ostream(response_.body())
         << "<html>\n"
@@ -136,9 +123,7 @@ private:
         << " requests so far.</p>\n"
         << "</body>\n"
         << "</html>\n";
-    }
-    else if (request_.target() == "/time")
-    {
+    } else if (request_.target() == "/time") {
       response_.set(http::field::content_type, "text/html");
       beast::ostream(response_.body())
         << "<html>\n"
@@ -150,9 +135,7 @@ private:
         << " seconds since the epoch.</p>\n"
         << "</body>\n"
         << "</html>\n";
-    }
-    else
-    {
+    } else {
       response_.result(http::status::not_found);
       response_.set(http::field::content_type, "text/plain");
       beast::ostream(response_.body()) << "File not found\r\n";
@@ -160,9 +143,8 @@ private:
   }
 
   void create_post_response() {
-    if (request_.target() == "/email")
-    {
-      auto& body = this->request_.body();
+    if (request_.target() == "/email") {
+      auto &body = this->request_.body();
       auto body_str = boost::beast::buffers_to_string(body.data());
       std::cout << "receive body is " << body_str << std::endl;
       this->response_.set(http::field::content_type, "text/json");
@@ -175,7 +157,7 @@ private:
         root["error"] = 1001;
         std::string jsonstr = root.toStyledString();
         beast::ostream(this->response_.body()) << jsonstr;
-        return ;
+        return;
       }
 
       auto email = src_root["email"].asString();
@@ -186,9 +168,7 @@ private:
       root["msg"] = "recevie email post success";
       std::string jsonstr = root.toStyledString();
       beast::ostream(this->response_.body()) << jsonstr;
-    }
-    else
-    {
+    } else {
       response_.result(http::status::not_found);
       response_.set(http::field::content_type, "text/plain");
       beast::ostream(response_.body()) << "File not found\r\n";
@@ -197,8 +177,7 @@ private:
 
   // Asynchronously transmit the response message.
   void
-  write_response()
-  {
+  write_response() {
     auto self = shared_from_this();
 
     response_.content_length(response_.body().size());
@@ -206,8 +185,7 @@ private:
     http::async_write(
       socket_,
       response_,
-      [self](beast::error_code ec, std::size_t)
-      {
+      [self](beast::error_code ec, std::size_t) {
         self->socket_.shutdown(tcp::socket::shutdown_send, ec);
         self->deadline_.cancel();
       });
@@ -215,15 +193,12 @@ private:
 
   // Check whether we have spent enough time on this connection.
   void
-  check_deadline()
-  {
+  check_deadline() {
     auto self = shared_from_this();
 
     deadline_.async_wait(
-      [self](beast::error_code ec)
-      {
-        if (!ec)
-        {
+      [self](beast::error_code ec) {
+        if (!ec) {
           // Close socket to cancel any outstanding operation.
           self->socket_.close(ec);
         }
@@ -233,11 +208,9 @@ private:
 
 // "Loop" forever accepting new connections.
 void
-http_server(tcp::acceptor& acceptor, tcp::socket& socket)
-{
+http_server(tcp::acceptor &acceptor, tcp::socket &socket) {
   acceptor.async_accept(socket,
-                        [&](beast::error_code ec)
-                        {
+                        [&](beast::error_code ec) {
                           if (!ec)
                             std::make_shared<http_connection>(std::move(socket))->start();
                           http_server(acceptor, socket);
@@ -245,10 +218,8 @@ http_server(tcp::acceptor& acceptor, tcp::socket& socket)
 }
 
 int
-main(int argc, char* argv[])
-{
-  try
-  {
+main(int argc, char *argv[]) {
+  try {
     // Check command line arguments.
     /*if (argc != 3)
     {
@@ -263,16 +234,15 @@ main(int argc, char* argv[])
     auto const address = net::ip::make_address("127.0.0.1");
     unsigned short port = static_cast<unsigned short>(8080);
 
-    net::io_context ioc{ 1 };
+    net::io_context ioc{1};
 
-    tcp::acceptor acceptor{ ioc, {address, port} };
-    tcp::socket socket{ ioc };
+    tcp::acceptor acceptor{ioc, {address, port}};
+    tcp::socket socket{ioc};
     http_server(acceptor, socket);
 
     ioc.run();
   }
-  catch (std::exception const& e)
-  {
+  catch (std::exception const &e) {
     std::cerr << "Error: " << e.what() << std::endl;
     return EXIT_FAILURE;
   }
