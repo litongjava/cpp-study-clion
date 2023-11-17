9350b31f930e2f29b3c8033c772c01e9b880fa40
litongmacos <litongjava@qq.com>
2023-11-17 13:35:08 -1000

    format beast-web-scoket-demo2

--- cmake-boost-beast-websocket-demo02/WebSocketServer.cpp
+++ cmake-boost-beast-websocket-demo02/WebSocketServer.cpp
@@ -1,14 +1,16 @@
 #include "WebSocketServer.h"
 
-WebSocketServer::WebSocketServer(net::io_context &ioc, unsigned short port) : _ioc(ioc),
-                                                                              _acceptor(ioc, net::ip::tcp::endpoint(
-                                                                                net::ip::tcp::v4(), port)) {
+WebSocketServer::WebSocketServer(net::io_context &ioc, unsigned short port)
+  : _ioc(ioc),
+    _acceptor(ioc, net::ip::tcp::endpoint(net::ip::tcp::v4(), port)){
   std::cout << "Server start on port : " << port << std::endl;
 }
 
 void WebSocketServer::StartAccept() {
   auto con_ptr = std::make_shared<Connection>(_ioc);
-  _acceptor.async_accept(con_ptr->GetSocket(), [this, con_ptr](error_code err) {
+  boost::asio::ip::tcp::socket &socket = con_ptr->GetSocket();
+
+  auto token = [this, con_ptr](error_code err) {
     try {
       if (!err) {
         con_ptr->AsyncAccept();
@@ -23,5 +25,7 @@ void WebSocketServer::StartAccept() {
     catch (std::exception &exp) {
       std::cout << "async_accept error is " << exp.what() << std::endl;
     }
-  });
+  };
+
+  _acceptor.async_accept(socket, token);
 }
--- cmake-boost-beast-websocket-demo02/demo02.cpp
+++ cmake-boost-beast-websocket-demo02/demo02.cpp
@@ -1,8 +1,8 @@
 #include "WebSocketServer.h"
 
 int main(){
-	net::io_context ioc;
-	WebSocketServer server(ioc, 10086);
-	server.StartAccept();
-	ioc.run();
+  net::io_context ioc;
+  WebSocketServer server(ioc, 10086);
+  server.StartAccept();
+  ioc.run();
 }
