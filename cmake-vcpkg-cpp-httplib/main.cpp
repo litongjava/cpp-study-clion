f110391e2c2617c0068184c087b2214263e43401
litongjava <litongjava@qq.com>
2024-04-30 14:16:13 -1000

    update main.cpp

--- cmake-vcpkg-cpp-httplib/main.cpp
+++ cmake-vcpkg-cpp-httplib/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <httplib.h>
+
+void handler(const httplib::Request &req, httplib::Response &res) {
+  res.set_content("Hello, World!", "text/plain");
+};
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  int port = 8080;
+  printf("Server is running on port %d\n", port);
+  httplib::Server svr;
+
+  svr.Get("/", handler);
+
+  if (!svr.listen("0.0.0.0", port)) {
+    std::cerr << "Error starting server on port " << port << std::endl;
+    return 1;
+  }
+
   return 0;
 }
